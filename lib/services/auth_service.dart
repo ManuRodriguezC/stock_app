cc9d90e097fefe698d9cb72e55914610d9c792c5
Manudev <estebanclimb@gmail.com>
2026-09-04 22:15:57 -0500

    Split Auth service to autenticate user

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,25 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AuthService {
+  final SupabaseClient _supabase;
+
+  AuthService(this._supabase);
+
+  Future<AuthResponse> signIn({
+    required String email,
+    required String password,
+  }) async {
+    return await _supabase.auth.signInWithPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  Future<void> signOut() async {
+    await _supabase.auth.signOut();
+  }
+
+  User? get currentUser => _supabase.auth.currentUser;
+
+  Session? get currentSession => _supabase.auth.currentSession;
+}
