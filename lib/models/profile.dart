4a18c4bc9286e7c5022485983e65acdca71a66f3
Manudev <estebanclimb@gmail.com>
2026-09-04 20:42:06 -0500

    T02-Define models

---
+++ lib/models/profile.dart
@@ -0,0 +1,13 @@
+class Profile {
+  final String id;
+  final String name;
+  final String email;
+  final DateTime createdAt;
+
+  Profile({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.createdAt,
+  });
+}
