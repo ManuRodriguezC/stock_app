d621699b3d8e1e470975a11a42ab0300e6ce7fa8
Manudev <estebanclimb@gmail.com>
2026-09-04 21:51:47 -0500

    Add T03 - conexion with supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: '.env');
+
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    publishableKey: dotenv.env['SUPABASE_KEY']!,
+  );
+
+  final supabase = Supabase.instance.client;
+
+  debugPrint('=================================');
+  debugPrint('Supabase inicializado correctamente');
+  debugPrint('Cliente $supabase');
+  debugPrint('=================================');
+
   runApp(const MainApp());
 }
 
